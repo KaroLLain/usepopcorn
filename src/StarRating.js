16eceb86d1f9f882fe236252f04c90c41a294599
KaroLLain <39537241+KaroLLain@users.noreply.github.com>
2024-02-21 14:28:44 +0100

    com

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const containerStyle = {
   display: 'flex',
@@ -8,6 +9,15 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: 'flex',
+};
+
+StarRating.propTypes = {
+  maxrating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
 };
 
 export default function StarRating({ maxRating = 5, color = '#fcc419', size = 48, className = '', messages = [], defaultRating = 0 }) {
