4a10212192b05a1f0fbb189c988c67f40bba441a
KaroLLain <39537241+KaroLLain@users.noreply.github.com>
2024-02-21 13:35:40 +0100

    comm

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 
+import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */} <StarRating maxRating={5} messages={['Teribble', 'Bad', 'Was OK', 'Good', 'Amazing']} />
+    <StarRating color="pink" size={50} className="test" defaultRating={3} />
   </React.StrictMode>
 );
